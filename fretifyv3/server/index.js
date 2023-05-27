05d542b1f40e5b9a34d2dbec78c88180a1f69add
Steven Borik <SBD2RP@hermanmiller.com>
2023-05-27 03:20:22 -0400

    refresh token logic

--- fretifyv3/server/index.js
+++ fretifyv3/server/index.js
@@ -1,17 +1,17 @@
-const express = require('express')
+const express = require('express');
 const request = require('request');
 const dotenv = require('dotenv');
+const bodyParser = require('body-parser');
 
-const port = 5000
 
-global.access_token = ''
+const port = 5000;
 
-dotenv.config()
+dotenv.config();
 
-var spotify_client_id = process.env.SPOTIFY_CLIENT_ID
-var spotify_client_secret = process.env.SPOTIFY_CLIENT_SECRET
+var spotify_client_id = process.env.SPOTIFY_CLIENT_ID;
+var spotify_client_secret = process.env.SPOTIFY_CLIENT_SECRET;
 
-var spotify_redirect_uri = 'http://localhost:3000/auth/callback'
+var spotify_redirect_uri = 'http://localhost:3000/auth/callback';
 
 var generateRandomString = function (length) {
   var text = '';
@@ -25,24 +25,28 @@ var generateRandomString = function (length) {
 
 var app = express();
 
-app.get('/auth/login', (req, res) => {
+app.use(express.json());
 
-  var scope = "streaming user-read-email user-read-private"
+
+let access_token = '';
+let refresh_token = '';
+
+app.get('/auth/login', (req, res) => {
+  var scope = 'streaming user-read-email user-read-private';
   var state = generateRandomString(16);
 
   var auth_query_parameters = new URLSearchParams({
-    response_type: "code",
+    response_type: 'code',
     client_id: spotify_client_id,
     scope: scope,
     redirect_uri: spotify_redirect_uri,
-    state: state
-  })
+    state: state,
+  });
 
   res.redirect('https://accounts.spotify.com/authorize/?' + auth_query_parameters.toString());
-})
+});
 
 app.get('/auth/callback', (req, res) => {
-
   var code = req.query.code;
 
   var authOptions = {
@@ -50,28 +54,52 @@ app.get('/auth/callback', (req, res) => {
     form: {
       code: code,
       redirect_uri: spotify_redirect_uri,
-      grant_type: 'authorization_code'
+      grant_type: 'authorization_code',
     },
     headers: {
-      'Authorization': 'Basic ' + (Buffer.from(spotify_client_id + ':' + spotify_client_secret).toString('base64')),
-      'Content-Type' : 'application/x-www-form-urlencoded'
+      Authorization: 'Basic ' + Buffer.from(spotify_client_id + ':' + spotify_client_secret).toString('base64'),
+      'Content-Type': 'application/x-www-form-urlencoded',
     },
-    json: true
+    json: true,
   };
 
-  request.post(authOptions, function(error, response, body) {
+  request.post(authOptions, function (error, response, body) {
     if (!error && response.statusCode === 200) {
       access_token = body.access_token;
-      res.redirect('/')
+      refresh_token = body.refresh_token;
+      res.redirect('/');
     }
   });
-
-})
+});
 
 app.get('/auth/token', (req, res) => {
-  res.json({ access_token: access_token})
-})
+  res.json({ access_token: access_token, refresh_token: refresh_token });
+});
+
+app.post('/auth/refresh', (req, res) => {
+  const { refresh_token } = req.body;
+
+  var authOptions = {
+    url: 'https://accounts.spotify.com/api/token',
+    form: {
+      grant_type: 'refresh_token',
+      refresh_token: refresh_token,
+    },
+    headers: {
+      Authorization: 'Basic ' + Buffer.from(spotify_client_id + ':' + spotify_client_secret).toString('base64'),
+      'Content-Type': 'application/x-www-form-urlencoded',
+    },
+    json: true,
+  };
+
+  request.post(authOptions, function (error, response, body) {
+    if (!error && response.statusCode === 200) {
+      access_token = body.access_token;
+      res.json({ access_token: access_token });
+    }
+  });
+});
 
 app.listen(port, () => {
-  console.log(`Listening at http://localhost:${port}`)
-})
+  console.log(`Listening at http://localhost:${port}`);
+});
--- fretifyv3/src/App.js
+++ fretifyv3/src/App.js
@@ -4,24 +4,45 @@ import Login from './Login'
 import './App.css';
 
 function App() {
-
   const [token, setToken] = useState('');
+  const [refreshToken, setRefreshToken] = useState('');
+  const [expiresInMilliseconds, setExpiresInMilliseconds] = useState(0);
 
   useEffect(() => {
-
     async function getToken() {
       const response = await fetch('/auth/token');
       const json = await response.json();
       setToken(json.access_token);
+      setRefreshToken(json.refresh_token);
+      setExpiresInMilliseconds(json.expires_in * 1000); // Convert seconds to milliseconds
     }
 
     getToken();
-
   }, []);
+
+  const refreshAccessToken = async () => {
+    const response = await fetch('/auth/refresh', {
+      method: 'POST',
+      body: JSON.stringify({ refresh_token: refreshToken }),
+      headers: {
+        'Content-Type': 'application/json'
+      }
+    });
+
+    const json = await response.json();
+    setToken(json.access_token);
+    setExpiresInMilliseconds(json.expires_in * 1000); // Convert seconds to milliseconds
+  };
+
+  useEffect(() => {
+    const expirationTimeout = setTimeout(refreshAccessToken, expiresInMilliseconds);
+
+    return () => clearTimeout(expirationTimeout);
+  }, [token, expiresInMilliseconds]);
 
   return (
     <>
-        { (token === '') ? <Login/> : <WebPlayback token={token} /> }
+      {(token === '') ? <Login /> : <WebPlayback token={token} />}
     </>
   );
 }
